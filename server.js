23567c94d3f7a0720c2d950a920c709d4803a471
BhaveshG-22 <107833962+BhaveshG-22@users.noreply.github.com>
2023-03-09 21:59:17 -0500

    Integrated MongoDB with basic frontend

--- server.js
+++ server.js
@@ -7,13 +7,46 @@ const multer = require('multer')
 
 var app = express()
 
+const { connectToDb, getDb } = require('./db')
+const { ObjectId } = require('mongodb')
+
+// db connection
+let db
+connectToDb((err) => {
+    if (!err) {
+        app.listen(process.env.PORT || 3000, () => {
+        })
+        db = getDb()
+    }
+})
+
+// const port = process.env.PORT || 3000
+// app.listen(port, function () {
+//     console.log('Your app is listening on port ' + port)
+// })
+
 app.use(cors())
 app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.render(path.join(__dirname + '/views/index.ejs'))
+
+    let blogs = []
+    db.collection('blogs')
+        .find()
+        .sort({ _id: -1 })
+        .forEach(blog => blogs.push(blog))
+        .then(() => {
+
+            res.render(path.join(__dirname + '/views/index.ejs'), { articles: blogs })
+            // res.render('index', { articles: blogs })
+        })
+        .catch((err) => {
+            res.status(500).json({ error: 'could not get data' })
+        })
+
+    // res.render(path.join(__dirname + '/views/index.ejs'))
 })
 
 // app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
@@ -26,9 +59,6 @@ app.get('/', (req, res) => {
 //   })
 // })
 
-const port = process.env.PORT || 3000
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-})
+
 
 module.exports = app
